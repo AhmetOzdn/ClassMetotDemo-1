0b8d620c302ad199bc4e4ac6b9fc27ccc8000971
HP <HP@DESKTOP-3U1RFUH>
2021-07-21 15:14:38 +0300

    Add project files.

---
+++ ClassMetotDemo-1/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo_1
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri Musteriler)
+        {
+            Console.WriteLine("Başarıyla Eklendi : "+ Musteriler.Ad + " - " + Musteriler.Soyad);
+        }
+
+        public void Sil(Musteri Musteriler) 
+        {
+            Console.WriteLine("Başarılaya Silindi : "+Musteriler.Ad + " " + Musteriler.Soyad);
+        }
+
+        public void List(Musteri Musteriler)
+        {
+            
+            Console.WriteLine("Kişi Başarıyla Listelendi : "+Musteriler.Ad + " " + Musteriler.Soyad);
+        }
+
+        
+    }
+}
---
+++ ClassMetotDemo-1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassMetotDemo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri Musteri1 = new Musteri();
+            Musteri1.Ad = "Ahmet" ;
+            Musteri1.Soyad = "OZDEN" ;
+            Musteri1.TCNo = 2222 ;
+            Musteri1.Id = 111111;
+            Musteri1.KartNo = 999999999;
+
+
+            Musteri Musteri2 = new Musteri();
+            Musteri2.Ad = "Mehmet";
+            Musteri2.Soyad = "OZDEN";
+            Musteri2.TCNo = 1111;
+            Musteri2.Id = 333333;
+            Musteri2.KartNo = 777777777;
+
+
+            Musteri Musteri3 = new Musteri();
+            Musteri3.Ad = "Hüseyin";
+            Musteri3.Soyad = "OZDEN";
+            Musteri3.TCNo = 3333;
+            Musteri3.Id = 222222;
+            Musteri3.KartNo = 555555555;
+
+            Musteri[] MusteriList = new Musteri[] {Musteri1,Musteri2,Musteri3};
+
+            foreach (var Musteri in MusteriList)
+            {
+                Console.WriteLine(Musteri.Ad +" "+Musteri.Soyad +" "+Musteri.Id +" "+Musteri.KartNo +" "+Musteri.TCNo);
+            }
+
+            MusteriManager Musterimanager = new MusteriManager();
+            Musterimanager.Ekle(Musteri1);
+            Musterimanager.Sil(Musteri2);
+            Musterimanager.List(Musteri3);
+        }
+    }
+}
